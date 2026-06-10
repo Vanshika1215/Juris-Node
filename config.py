4f32b007f7fe88290ed1f680ae0d51642c0ab2b6
Vanshika1215 <2k23.2313743cs@gmail.com>
2026-06-10 21:18:05 +0530

    Updated config

--- config.py
+++ config.py
@@ -8,9 +8,11 @@ SUPABASE_URL: str = os.getenv("SUPABASE_URL", "")
 SUPABASE_KEY: str = os.getenv("SUPABASE_KEY", "")
 DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 
+
 # Simple configuration validations
 if not SUPABASE_URL or not SUPABASE_KEY:
     print("Warning: Supabase credentials are empty or missing inside .env context.")
 
 if not DATABASE_URL:
+
     print("Warning: DATABASE_URL target connection string is missing inside .env context.")
